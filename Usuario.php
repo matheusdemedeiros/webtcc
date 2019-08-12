6c7de12033ed1a7eff4c115a0e373a9f2e87439b
lucas2610 <lucaschaves2610@gmail.com>
2019-08-12 11:06:58 -0300

    GitKraken
    Teste commit

---
+++ Usuario.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * 
+ */
+class Usuario
+{
+	protected $email;
+	protected $senha;
+
+	public function __construct($email, $senha)
+	{
+		$this->email=$email;
+		$this->senha=$senha;
+	}
+	
+	public function setEmail($email)
+	{
+		$this->email =$email;
+	}
+
+	public function getEmail()
+	{
+		return $this->email;
+	}
+
+	public function setSenha($senha)
+	{
+		$this->senha =$senha;
+	}
+
+	public function getSenha()
+	{
+		return $this->senha;
+	}
+}
---
+++ teste_tabela.php
@@ -0,0 +1,31 @@
+<?php
+
+include("conecta.php");
+
+require_once "Usuario.php";
+require_once "Aluno.php";
+
+
+//$aluno = new Aluno($_POST['email'], $_POST['password'] , $_POST['nome']);
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$senha = $_POST['password'];
+
+//mysqli_query($conexao,"insert into aluno(nome, email,senha) values($aluno->getNome(),$aluno->getEmail(),$aluno->getSenha())");
+
+
+
+mysqli_query($conexao,"insert into aluno(nome, email,senha) values('$nome', '$email','$senha')");
+ 
+
+/*
+echo $aluno->getNome();
+echo $aluno->getEmail();
+echo $aluno->getSenha();
+*/
+
+
+
+
+//list($nome, $email, $password) = $info;
+// echo "$nome é o nome do aluno que tem esse email $email e essa senha: $password.\n";
