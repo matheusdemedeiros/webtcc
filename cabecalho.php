7f13524d5a9d6a99a93bfcaecbb7b4e609c9bda4
lucas2610 <lucaschaves2610@gmail.com>
2019-08-26 10:31:05 -0300

    Iniciei o materialize

--- cabecalho.php
+++ cabecalho.php
@@ -1,13 +1,18 @@
 <head>
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<!-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
+<!-- Compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        
 </head>
-<header class="container">
+<header>
 	<title><?php print $cabecalho_title; ?></title>
 	
 		<img src="img/ifsc.jpg">
 	
-	<nav class="menu-opcoes">
+	<nav class="menu-opcoes" style = "background-color: #00e676">
         <ul>
             <li><a href="cadastroaluno.php">Cadastrar Aluno</a></li>
             <li><a href="cadastrocurso.php">Cadastrar Curso</a></li>
--- cadastroaluno.php
+++ cadastroaluno.php
@@ -4,30 +4,31 @@
 <head>
     <meta charset="UTF-8">
     <title>Pagina de Cadastro</title>
-  
+
     <link rel="stylesheet" href="css/estilos.css">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <script type ="text/javascript">
-        function validar(){
+    <!-- <link rel="stylesheet" href="css/bootstrap.css"> -->
+    <script type="text/javascript">
+        function validar() {
             var senha = formuser.password.value;
-            var confirmarsenha= formuser.password2.value;
-            if(senha == "" || senha.length <=5){
+            var confirmarsenha = formuser.password2.value;
+            if (senha == "" || senha.length <= 5) {
                 alert('prencha o campo com no mínimo 6 caracteres');
                 formuser.password.focus();
                 return false;
             }
-            if(confirmarsenha == "" || confirmarsenha.length <=5){
+            if (confirmarsenha == "" || confirmarsenha.length <= 5) {
                 alert('prencha o campo com no mínimo 6 caracteres');
                 formuser.password2.focus();
                 return false;
             }
 
-            if(senha!=confirmarsenha){
+            if (senha != confirmarsenha) {
                 alert('Senhas diferentes!');
                 formuser.password2.focus();
                 return false;
             }
-}
+        }
+
     </script>
 </head>
 
@@ -36,45 +37,51 @@
 		$cabecalho_title = "Cadastro Aluno";
 		include("cabecalho.php"); 
 		?>
+
     <div class="container">
-        <form name="formuser" action="inserirAluno.php" method="POST">
 
-            <fieldset>
-                <legend>Cadastro Aluno</legend>
-                <label for="nome">Nome completo</label>
-                <div>
-                    
-                    <input type="text" id="nome" name="nome" autofocus required>
-                </div>
+        <form class="container" name="formuser" action="inserirAluno.php" method="POST">
 
-                <label for="email">Email</label>
-                <div>
-                    
-                    <div class="input-group">
-                        <span class="input-group-addon">@</span>
-                        <input type="email" id="email" name="email" autofocus required>
+            <fieldset class="container">
+                <h5>Cadastro Aluno</h5>
+
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input type="text" id="nome" name="nome" autofocus required>
+                        <label for="nome">Nome completo</label>
                     </div>
                 </div>
-                 <label for="senha">Senha</label>
-                <div>
-                   
-                    <input type="password" id="password" name="password" autofocus required>
-                </div>
-                <label for="senha2">Confirmar Senha</label>
-                <div>
-                    
-                    <input type="password" id="password2" name="password2" autofocus required>
+
+                <div class="row">
+                    <div class="input-field col s12">
+
+                        <input type="email" id="email" name="email" autofocus required>
+                        <label for="email">Email</label>
+                    </div>
                 </div>
 
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input type="password" id="password" name="password" autofocus required>
+                        <label for="senha">Senha</label>
+                    </div>
+                </div>
 
 
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input type="password" id="password2" name="password2" autofocus required>
+                        <label for="senha2">Confirmar Senha</label>
+                    </div>
+                </div>
 
-                <input type="submit" name="Cadastrar" value="Cadastrar"  onclick="return validar()" />
+
+                <input class="btn" style="background-color: #00e676" type="submit" name="Cadastrar" value="Cadastrar" onclick="return validar()" />
             </fieldset>
 
         </form>
     </div>
-<!--    <?php include("rodape.php"); ?>-->
+    <!--    <?php include("rodape.php"); ?>-->
 </body>
 
 </html>
