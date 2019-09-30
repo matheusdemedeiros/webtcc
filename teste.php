5e7223580255f39e4069a63a87e3de70d8d3860b
lucas2610 <lucaschaves2610@gmail.com>
2019-09-30 07:55:33 -0300

    gerador pdf iniciado

--- teste.php
+++ teste.php
@@ -1,6 +1,6 @@
 <?php
 
-$file_url = $_GET['teste2.php'];
+$file_url = $_GET['declaracao.php?id=29'];
 header('Content-Type: application/octet-stream');
 header("Content-Transfer-Encoding: Binary"); 
 header("Content-disposition: attachment; filename=\"Declaração.pdf" . basename($file_url) . "\""); 
