3f6e4fb8e7e38ef9599cfdf9ffc26ccf4c47b46e
lucas2610 <lucaschaves2610@gmail.com>
2019-10-18 11:39:24 -0300

    mensagem de email senha.

--- validaLogin.php
+++ validaLogin.php
@@ -6,7 +6,7 @@
     $sql =  "select * from users where Email ='$recemail' limit 1";
     $result_sql = mysqli_query($conexao, $sql);
     $result_sql2=$result_sql;
-    $hashPassword=mysqli_fetch_array($result_sql2);
+    $userData=mysqli_fetch_array($result_sql2);
     $rows = mysqli_num_rows($result_sql);
     
     if ($rows == ''){
@@ -14,40 +14,42 @@
 
 <?php 
        echo "<script> alert('Erro: e-mail e/ou senha incorretos!!');</script>";
-       header("Location:Index.php"); 
+      echo "<script> window.location='Index.php';</script>";
+      
+       //header("Location:Index.php"); 
         ?>
 <?php
     }else{
-        $confirmacao_senha=password_verify($recsenha,$hashPassword['Password']);
+        $confirmacao_senha=password_verify($recsenha,$userData['Password']);
         $senha_confirmada=0;
         if($confirmacao_senha==true){
             $senha_confirmada=1;
+        }else{
+            echo "<script> alert('Erro: e-mail e/ou senha incorretos!!');</script>";
+            echo "<script> window.location='Index.php';</script>";
         }
-        // var_dump($confirmacao_senha);
-        // var_dump($rows);
+      
         if($rows==1) {
           
             // echo "teste";
                if($senha_confirmada==1) {
-                $dados2=mysqli_fetch_array($result_sql2);
-                  echo $senha_confirmada;
-                  var_dump($hashPassword['TypeUsers']);
-                  var_dump($dados2['TypeUsers']);
-                   while($dados=mysqli_fetch_assoc($result_sql)){
+               
+                 
+                   while($userData){
                     session_start();
                    
-                            switch ($dados['TypeUsers']) {
+                            switch ($userData['TypeUsers']) {
                                 
                                 case "TermPaperTeacher":
-                                $_SESSION['name_session'] = $dados['IdUser'];
+                                $_SESSION['name_session'] = $userData['IdUser'];
                                 header("Location:listagemTCC.php");
                                 break;
                                 case "Student":
-                                $_SESSION['name_session'] = $dados['IdUser'];
+                                $_SESSION['name_session'] = $userData['IdUser'];
                                 header("Location:listagemTCCAlunos.php");
                                 break;
                                 case "Advisor":
-                                $_SESSION['name_session'] = $dados['IdUser'];    
+                                $_SESSION['name_session'] = $userData['IdUser'];    
                                 header("Location:listagemTCCOrientador.php");
                                 break;
                             }
